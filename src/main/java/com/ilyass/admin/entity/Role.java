46e034d302193ce736de76c73f3d913b8a0301aa
dell <dell@DESKTOP-4P8U8AR>
2024-06-02 20:10:06 +0100

    adding relations to Role entity

--- src/main/java/com/ilyass/admin/entity/Role.java
+++ src/main/java/com/ilyass/admin/entity/Role.java
@@ -3,15 +3,23 @@ package com.ilyass.admin.entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 
+@AllArgsConstructor
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long roleId;
     private String name;
+
+    private Set<User> users = new HashSet<User>();
 
     @Override
     public boolean equals(Object o) {
