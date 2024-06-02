fd9996eac127501b86a42895022902f4e773b4bf
dell <dell@DESKTOP-4P8U8AR>
2024-06-02 19:54:44 +0100

    adding relations to Student entity

--- src/main/java/com/ilyass/admin/entity/Student.java
+++ src/main/java/com/ilyass/admin/entity/Student.java
@@ -1,10 +1,7 @@
 package com.ilyass.admin.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -14,6 +11,7 @@ import java.util.Set;
 @Table(name = "activities")
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class Activity {
