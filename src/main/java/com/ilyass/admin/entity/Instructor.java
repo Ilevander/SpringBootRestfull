fbd6211637a318217c7dd289fd997cb3bd440b25
dell <dell@DESKTOP-4P8U8AR>
2024-06-03 01:18:34 +0100

    Model Layer: custom instructor entity @nnotations relation binding

--- src/main/java/com/ilyass/admin/entity/Instructor.java
+++ src/main/java/com/ilyass/admin/entity/Instructor.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "instructors")
@@ -17,21 +19,42 @@ import java.util.Objects;
 public class Instructor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instructor_id",nullable = false)
     private Long instructorId;
+
+    @Basic
+    @Column(name = "first_name",nullable = false,length = 45)
     private String firstName;
+
+    @Basic
+    @Column(name = "last_name",nullable = false,length = 45)
     private String lastName;
-    private String summury;
+
+    @Basic
+    @Column(name = "summary",nullable = false,length = 65)
+    private String summary;
+
+    //Only one instructor could have a group of activities
+    @OneToMany(mappedBy = "instructor",fetch = FetchType.LAZY)
+    private Set<Activity> activities = new HashSet<>();
+
+    //Only one Instructor could be an only one User (cascade->REMOVE:to remove also user during removing instructor)
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id",referencedColumnName = "user_id",nullable = false)
+    private User user;
+
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Instructor that = (Instructor) o;
-        return Objects.equals(instructorId, that.instructorId) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(summury, that.summury);
+        return Objects.equals(instructorId, that.instructorId) && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(summary, that.summary);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(instructorId, firstName, lastName, summury);
+        return Objects.hash(instructorId, firstName, lastName, summary);
     }
 }
