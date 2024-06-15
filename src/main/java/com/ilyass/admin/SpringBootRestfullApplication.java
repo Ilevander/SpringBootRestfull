bb6e86cfde9c16d4b40979fbce1285c4e3a33e85
dell <dell@DESKTOP-4P8U8AR>
2024-06-15 10:42:34 +0100

    Model Layer: Testing userOperations using CommandLineRunner

--- src/main/java/com/ilyass/admin/SpringBootRestfullApplication.java
+++ src/main/java/com/ilyass/admin/SpringBootRestfullApplication.java
@@ -1,13 +1,42 @@
 package com.ilyass.admin;
 
+import com.ilyass.admin.dao.*;
+import com.ilyass.admin.utility.OperationUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRestfullApplication {
+public class SpringBootRestfullApplication implements CommandLineRunner {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private ActivityDao activityDao;
+
+    @Autowired
+    private InstructorDao instructorDao;
+
+    @Autowired
+    private StudentDao studentDao;
+
+    @Autowired
+    private RoleDao roleDao;
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestfullApplication.class, args);
     }
 
+    /**
+     * To Test Operation Utilities for All Entities
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        OperationUtility.usersOperations(userDao);
+    }
 }
