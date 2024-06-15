ee20676be991f43ec88496bca8dd959f0813e4e6
dell <dell@DESKTOP-4P8U8AR>
2024-06-15 23:55:29 +0100

    Business Layer:Creating DTOs and Mappers: ActivityMapper

---
+++ src/main/java/com/ilyass/admin/mapper/ActivityMapper.java
@@ -0,0 +1,28 @@
+package com.ilyass.admin.mapper;
+
+import com.ilyass.admin.dto.ActivityDTO;
+import com.ilyass.admin.entity.Activity;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityMapper {
+
+    private final InstructorMapper instructorMapper;
+
+    public ActivityMapper(InstructorMapper instructorMapper) {
+        this.instructorMapper = instructorMapper;
+    }
+
+    public ActivityDTO fromActivity(Activity activity) {
+            ActivityDTO activityDTO = new ActivityDTO();
+            BeanUtils.copyProperties(activity, activityDTO);
+            activityDTO.setInstructor(instructorMapper.fromInstructor(activity.getInstructor()));
+            return activityDTO;
+        }
+    public Activity fromActivityDTO(ActivityDTO activityDTO) {
+        Activity activity = new Activity();
+        BeanUtils.copyProperties(activityDTO, activity);
+        return activity;
+    }
+}
