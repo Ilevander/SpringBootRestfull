45d256ddd4c587019a89fd6b52a321137b7c453f
dell <dell@DESKTOP-4P8U8AR>
2024-06-15 22:18:41 +0100

    Business Layer:Creating DTOs and Mappers: ActivityDTO

---
+++ src/main/java/com/ilyass/admin/dto/ActivityDTO.java
@@ -0,0 +1,49 @@
+package com.ilyass.admin.dto;
+
+public class ActivityDTO {
+    private Long activityId;
+    private String activityName;
+    private String activityDuration;
+    private String activityDescription;
+    private InstructorDTO instructor;
+
+    public Long getActivityId() {
+        return activityId;
+    }
+
+    public void setActivityId(Long activityId) {
+        this.activityId = activityId;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public String getActivityDuration() {
+        return activityDuration;
+    }
+
+    public void setActivityDuration(String activityDuration) {
+        this.activityDuration = activityDuration;
+    }
+
+    public String getActivityDescription() {
+        return activityDescription;
+    }
+
+    public void setActivityDescription(String activityDescription) {
+        this.activityDescription = activityDescription;
+    }
+
+    public InstructorDTO getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(InstructorDTO instructor) {
+        this.instructor = instructor;
+    }
+}
