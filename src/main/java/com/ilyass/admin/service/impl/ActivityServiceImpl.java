b585f317fb9b9e11f630074cd281a0f18d04b14b
dell <dell@DESKTOP-4P8U8AR>
2024-06-16 03:32:10 +0100

    Business Layer: Service Layer: ActivityServiceImpl: updateActivity , findActivitiesByActivityName assignStudentToActivity

--- src/main/java/com/ilyass/admin/service/impl/ActivityServiceImpl.java
+++ src/main/java/com/ilyass/admin/service/impl/ActivityServiceImpl.java
@@ -2,26 +2,37 @@ package com.ilyass.admin.service.impl;
 
 import com.ilyass.admin.dao.ActivityDao;
 import com.ilyass.admin.dao.InstructorDao;
+import com.ilyass.admin.dao.StudentDao;
 import com.ilyass.admin.dto.ActivityDTO;
 import com.ilyass.admin.entity.Activity;
 import com.ilyass.admin.entity.Instructor;
+import com.ilyass.admin.entity.Student;
 import com.ilyass.admin.mapper.ActivityMapper;
 import com.ilyass.admin.service.ActivityService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import java.util.stream.Collectors;
 
+@Service
+@Transactional
 public class ActivityServiceImpl implements ActivityService {
 
     //Inject ActivityDao , ActivityMapper , InstructorDao
     private ActivityDao activityDao;
     private ActivityMapper activityMapper;
     private InstructorDao instructorDao;
+    private StudentDao studentDao;
 
-    public ActivityServiceImpl(ActivityDao activityDao,ActivityMapper activityMapper,InstructorDao instructorDao) {
+    public ActivityServiceImpl(ActivityDao activityDao,ActivityMapper activityMapper,InstructorDao instructorDao, StudentDao studentDao) {
         this.activityDao = activityDao;
         this.activityMapper = activityMapper;
         this.instructorDao = instructorDao;
+        this.studentDao = studentDao;
     }
 
 
@@ -41,17 +52,27 @@ public class ActivityServiceImpl implements ActivityService {
 
     @Override
     public ActivityDTO updateActivity(ActivityDTO activityDTO) {
-        return null;
+        Activity loadedActivity = loadActivityById(activityDTO.getActivityId());
+        Instructor instructor = instructorDao.findById(activityDTO.getInstructor().getInstructorId()).orElseThrow(() -> new EntityNotFoundException("Instructor With ID " + activityDTO.getInstructor().getInstructorId() + " Not Found"));
+        Activity activity = activityMapper.fromActivityDTO(activityDTO);
+        activity.setInstructor(instructor);
+        activity.setStudents(loadedActivity.getStudents());
+        Activity updatedActivity = activityDao.save(activity);
+        return activityMapper.fromActivity(updatedActivity);
     }
 
     @Override
     public Page<ActivityDTO> findActivitiesByActivityName(String keyword, int page, int size) {
-        return null;
+        PageRequest pageRequest = PageRequest.of(page,size);
+        Page<Activity> activityPage = activityDao.findActivitiesByTitleContains(keyword,pageRequest);
+        return new PageImpl<>(activityPage.getContent().stream().map(activity -> activityMapper.fromActivity(activity)).collect(Collectors.toList()));
     }
 
     @Override
     public void assignStudentToActivity(Long activityId, Long studentId) {
-
+        Student student = studentDao.findById(studentId).orElseThrow(()->new EntityNotFoundException("Student With ID "+studentId+" Not Found"));
+        Activity activity = loadActivityById(activityId);
+        activity.assignStudentToActivity(student);
     }
 
     @Override
