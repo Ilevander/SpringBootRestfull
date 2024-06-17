94fac803d4bd57bd59852fe0648e2a8a2e0f7474
dell <dell@DESKTOP-4P8U8AR>
2024-06-17 03:44:59 +0100

    Business Layer: Testing Application with Runner Class : createAdmin,assigned to his role'Admin'

--- src/main/java/com/ilyass/admin/runner/MyRunner.java
+++ src/main/java/com/ilyass/admin/runner/MyRunner.java
@@ -1,6 +1,7 @@
 package com.ilyass.admin.runner;
 
 import com.ilyass.admin.service.RoleService;
+import com.ilyass.admin.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,13 +13,22 @@ public class MyRunner implements CommandLineRunner {
 
     @Autowired
     private RoleService roleService;
+
+    @Autowired
+    private UserService userService;
     
     @Override
     public void run(String... args) throws Exception {
         createRoles();
+        createAdmin();
     }
 
     private void createRoles() {
         Arrays.asList("Admin","Instructor","Student").forEach(role-> roleService.createRole(role));
+    }
+
+    private void createAdmin() {
+        userService.createUser("admin@gmail.com", "1234");
+        userService.assignRoleToUser("admin@gmail.com", "Admin");
     }
 }
