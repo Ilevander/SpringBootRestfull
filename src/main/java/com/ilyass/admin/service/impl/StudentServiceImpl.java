e9d946e18234d92b97d4ce7eb97da6341598b744
dell <dell@DESKTOP-4P8U8AR>
2024-06-17 03:15:36 +0100

    Business Layer: Service Layer: StudentServiceImpl

---
+++ src/main/java/com/ilyass/admin/service/impl/StudentServiceImpl.java
@@ -0,0 +1,84 @@
+package com.ilyass.admin.service.impl;
+
+import com.ilyass.admin.dao.StudentDao;
+import com.ilyass.admin.dto.StudentDTO;
+import com.ilyass.admin.entity.Activity;
+import com.ilyass.admin.entity.Student;
+import com.ilyass.admin.entity.User;
+import com.ilyass.admin.mapper.StudentMapper;
+import com.ilyass.admin.service.StudentService;
+import com.ilyass.admin.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import java.util.Iterator;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class StudentServiceImpl implements StudentService {
+
+    private StudentDao studentDao;
+
+    private StudentMapper studentMapper;
+
+    private UserService userService;
+
+    public StudentServiceImpl(StudentDao studentDao, StudentMapper studentMapper, UserService userService) {
+        this.studentDao = studentDao;
+        this.studentMapper = studentMapper;
+        this.userService = userService;
+    }
+
+    @Override
+    public Student loadStudentById(Long studentId) {
+        return studentDao.findById(studentId).orElseThrow(() -> new EntityNotFoundException("Student with ID " + studentId + " not found"));
+    }
+
+    @Override
+    public Page<StudentDTO> loadStudentsByName(String name, int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<Student> studentsPage = studentDao.findStudentsByName(name, pageRequest);
+        return new PageImpl<>(studentsPage.getContent().stream().map(student -> studentMapper.fromStudent(student)).collect(Collectors.toList()), pageRequest, studentsPage.getTotalElements());
+    }
+
+    @Override
+    public StudentDTO loadStudentByEmail(String email) {
+        return studentMapper.fromStudent(studentDao.findStudentByEmail(email));
+    }
+
+    @Override
+    public StudentDTO createStudent(StudentDTO studentDTO) {
+        User user = userService.createUser(studentDTO.getUser().getEmail(), studentDTO.getUser().getPassword());
+        userService.assignRoleToUser(user.getEmail(), "Student");
+        Student student = studentMapper.fromStudentDTO(studentDTO);
+        student.setUser(user);
+        Student savedStudent = studentDao.save(student);
+        return studentMapper.fromStudent(savedStudent);
+    }
+
+    @Override
+    public StudentDTO updateStudent(StudentDTO studentDTO) {
+        Student loadedStudent = loadStudentById(studentDTO.getStudentId());
+        Student student = studentMapper.fromStudentDTO(studentDTO);
+        student.setUser(loadedStudent.getUser());
+        student.setActivities(loadedStudent.getActivities());
+        Student updatedStudent = studentDao.save(student);
+        return studentMapper.fromStudent(updatedStudent);
+    }
+
+    @Override
+    public void removeStudent(Long studentId) {
+        Student student = loadStudentById(studentId);
+        Iterator<Activity> courseIterator = student.getActivities().iterator();
+        if (courseIterator.hasNext()) {
+            Activity activity = courseIterator.next();
+            activity.removeStudentFromActivity(student);
+        }
+        studentDao.deleteById(studentId);
+    }
+}
