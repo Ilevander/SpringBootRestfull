54a86e340a5ea2b3725e767af6e6c7e6a741d37d
dell <dell@DESKTOP-4P8U8AR>
2024-06-17 03:01:20 +0100

    Business Layer: Service Layer: StudentService

---
+++ src/main/java/com/ilyass/admin/service/StudentService.java
@@ -0,0 +1,21 @@
+package com.ilyass.admin.service;
+
+import com.ilyass.admin.dto.StudentDTO;
+import com.ilyass.admin.entity.Student;
+import org.springframework.data.domain.Page;
+
+public interface StudentService {
+
+    Student loadStudentById(Long studentId);
+
+    Page<StudentDTO> loadStudentsByName(String name, int page, int size);
+
+    StudentDTO loadStudentByEmail(String email);
+
+    StudentDTO createStudent(StudentDTO studentDTO);
+
+    StudentDTO updateStudent(StudentDTO studentDTO);
+
+    void removeStudent(Long studentId);
+}
+
