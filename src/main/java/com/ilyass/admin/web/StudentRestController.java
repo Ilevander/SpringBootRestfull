a67738ddaa30cd5d8756ccc9930f7d1fd3afdda4
dell <dell@DESKTOP-4P8U8AR>
2024-06-18 14:29:32 +0100

    Business Layer: Developping Rest APIs: StudentRestController: TESTING at Postman, works correctely

---
+++ src/main/java/com/ilyass/admin/web/StudentRestController.java
@@ -0,0 +1,68 @@
+package com.ilyass.admin.web;
+
+import com.ilyass.admin.dto.ActivityDTO;
+import com.ilyass.admin.dto.StudentDTO;
+import com.ilyass.admin.entity.User;
+import com.ilyass.admin.service.ActivityService;
+import com.ilyass.admin.service.StudentService;
+import com.ilyass.admin.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+public class StudentRestController {
+
+    private final StudentService studentService;
+    private final UserService userService;
+    private ActivityService activityService;
+
+    public StudentRestController(StudentService studentService, UserService userService, ActivityService activityService) {
+        this.studentService = studentService;
+        this.userService = userService;
+        this.activityService = activityService;
+    }
+
+
+    @GetMapping
+    public Page<StudentDTO> searchStudents(@RequestParam(name = "keyword", defaultValue = "") String keyword,
+                                           @RequestParam(name = "page", defaultValue = "0") int page,
+                                           @RequestParam(name = "size", defaultValue = "5") int size) {
+        return studentService.loadStudentsByName(keyword, page, size);
+    }
+
+    @DeleteMapping("/{studentId}")
+    public void deleteStudent(@PathVariable Long studentId) {
+        studentService.removeStudent(studentId);
+    }
+
+    @PostMapping
+    public StudentDTO saveStudent(@RequestBody StudentDTO studentDTO) {
+        User user = userService.loadUserByEmail(studentDTO.getUser().getEmail());
+        if (user != null) throw new RuntimeException("Email Already Exist");
+        return studentService.createStudent(studentDTO);
+    }
+
+    @PutMapping("/{studentId}")
+    public StudentDTO updateStudent(@RequestBody StudentDTO studentDTO, @PathVariable Long studentId) {
+        studentDTO.setStudentId(studentId);
+        return studentService.updateStudent(studentDTO);
+    }
+
+    @GetMapping("/{studentId}/activities")
+    public Page<ActivityDTO> activitiesByStudentId(@PathVariable Long studentId,
+                                                @RequestParam(name = "page", defaultValue = "0") int page,
+                                                @RequestParam(name = "size", defaultValue = "5") int size) {
+        return activityService.fetchActivitiesForStudent(studentId, page, size);
+    }
+
+    @GetMapping("/{studentId}/other-activities")
+    public Page<ActivityDTO> nonSubscribedActivitiesByStudentId(@PathVariable Long studentId,
+                                                           @RequestParam(name = "page", defaultValue = "0") int page,
+                                                           @RequestParam(name = "size", defaultValue = "5") int size) {
+        return activityService.fetchNonEnrolledInActivitiesForStudent(studentId, page, size);
+    }
+
+    @GetMapping("/find")
+    public StudentDTO loadStudentByEmail(@RequestParam(name = "email", defaultValue = "") String email) {
+        return studentService.loadStudentByEmail(email);
+    }
+}
