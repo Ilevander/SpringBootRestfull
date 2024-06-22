d8f98c84dbf90f1a04c7bb9dd0fcb82062a0c31c
dell <dell@DESKTOP-4P8U8AR>
2024-06-22 16:14:38 +0100

    Add @CrossOrigin to the RestControllers

--- src/main/java/com/ilyass/admin/web/ActivityRestController.java
+++ src/main/java/com/ilyass/admin/web/ActivityRestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/activities")
+@CrossOrigin("*")
 public class ActivityRestController {
 
     private ActivityService activityService;
--- src/main/java/com/ilyass/admin/web/UserRestController.java
+++ src/main/java/com/ilyass/admin/web/UserRestController.java
@@ -1,11 +1,13 @@
 package com.ilyass.admin.web;
 
 import com.ilyass.admin.service.UserService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 public class UserRestController {
 
     private UserService userService;
