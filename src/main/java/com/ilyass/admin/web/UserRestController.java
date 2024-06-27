fb5dc05b9f8fb42505731f64ee5926dbf31eaaad
dell <dell@DESKTOP-4P8U8AR>
2024-06-27 03:48:29 +0100

    Security Layer: Generate Access Token using Refresh Token 1

--- src/main/java/com/ilyass/admin/web/UserRestController.java
+++ src/main/java/com/ilyass/admin/web/UserRestController.java
@@ -1,5 +1,12 @@
 package com.ilyass.admin.web;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ilyass.admin.entity.User;
+import com.ilyass.admin.helper.JWTHelper;
 import com.ilyass.admin.service.UserService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,19 +14,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+import static com.ilyass.admin.constant.JWTUtil.AUTH_HEADER;
+import static com.ilyass.admin.constant.JWTUtil.SECRET;
+import com.ilyass.admin.entity.Role;
+
 @RestController
 @CrossOrigin("*")
 public class UserRestController {
 
     private UserService userService;
 
-    public UserRestController(UserService userService) {
+    private JWTHelper jwtHelper;
+
+
+    public UserRestController(UserService userService, JWTHelper jwtHelper) {
         this.userService = userService;
+        this.jwtHelper = jwtHelper;
     }
 
     @GetMapping("/users")
     @PreAuthorize("hasAuthority('Admin')")
     public boolean checkIfEmailExists(@RequestParam(name = "email",defaultValue = "") String email) {
         return userService.loadUserByEmail(email) != null;
+    }
+
+    @GetMapping("/refresh-token")
+    public void generateNewAccessToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String jwtRefreshToken = jwtHelper.extractTokenFromHeaderIfExists(request.getHeader(AUTH_HEADER));
+        if (jwtRefreshToken != null) {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET);
+            JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+            DecodedJWT decodedJWT = jwtVerifier.verify(jwtRefreshToken);
+            String email = decodedJWT.getSubject();
+            User user = userService.loadUserByEmail(email);
+            String jwtAccessToken = jwtHelper.generateAccessToken(user.getEmail(), user.getRoles().stream().map(Role::getName).collect(Collectors.toList()));
+            response.setContentType("application/json");
+            new ObjectMapper().writeValue(response.getOutputStream(), jwtHelper.getTokensMap(jwtAccessToken, jwtRefreshToken));
+        } else {
+            throw new RuntimeException("Refresh token required");
+        }
     }
 }
