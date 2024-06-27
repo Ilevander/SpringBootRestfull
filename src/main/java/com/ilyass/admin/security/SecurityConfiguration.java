1814cc596f573b9c3b73eecd540a57f9747d9412
dell <dell@DESKTOP-4P8U8AR>
2024-06-27 02:17:45 +0100

    Security Layer: Implementing UserDetailsService

--- src/main/java/com/ilyass/admin/security/SecurityConfiguration.java
+++ src/main/java/com/ilyass/admin/security/SecurityConfiguration.java
@@ -11,18 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
